674f7298cad3666433ea10f2d00abd9c02e5b848
zhengjianfeng <zhengjianfeng@github.com>
2022-08-16 17:13:12 +0800

    38. 外观数列

--- Arithmetic/LeetCode/src/s0038/Solution.java
+++ Arithmetic/LeetCode/src/s0038/Solution.java
@@ -1,13 +1,33 @@
 package s0038;
 
+
 /**
  * Õ‚π€–Ú¡–
- * 
- * @author zheng
  *
+ * @author zheng
  */
 class Solution {
-	public String countAndSay(int n) {
-		return null;
-	}
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+
+        String s = countAndSay(n - 1);
+
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count++;
+            if (i == s.length() - 1 || s.charAt(i) != s.charAt(i + 1)) {
+                sb.append(count);
+                sb.append(s.charAt(i));
+                count = 0;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().countAndSay(3));
+    }
 }
