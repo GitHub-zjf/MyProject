c1afa9d0dc6e0d6841e31da07188b9b506bebfe3
zhengjianfeng <zhengjianfeng@github.com>
2022-08-26 14:48:34 +0800

    342. 4的幂

---
+++ Arithmetic/LeetCode/src/s0342/Solution.java
@@ -0,0 +1,28 @@
+package s0342;
+
+/**
+ * 4µÄÃÝ
+ */
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        double pow = 0;
+        for (int i = 0; pow < n; i++) {
+            pow = Math.pow(4, i);
+            if (pow == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isPowerOfFour2(int n) {
+        return Integer.bitCount(n) == 1 && Integer.toBinaryString(n).length() % 2 == 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isPowerOfFour2(-1));
+    }
+}
