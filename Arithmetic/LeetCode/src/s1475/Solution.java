e33a5ed44412007d448a1b0867b6bd0cda5aa8ca
zhengjianfeng <zhengjianfeng@github.com>
2022-09-01 17:27:30 +0800

    1475. 商品折扣后的最终价格

---
+++ Arithmetic/LeetCode/src/s1475/Solution.java
@@ -0,0 +1,23 @@
+package s1475;
+
+/**
+ * 1475. 商品折扣后的最终价格
+ */
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int len = prices.length;
+        int[] ret = new int[len];
+        for (int i = 0; i < len; i++) {
+            int min = 0;
+            for (int j = i + 1; j < len; j++) {
+                if (prices[j] <= prices[i]) {
+                    min = prices[j];
+                    break;
+                }
+            }
+            ret[i] = prices[i] - min;
+        }
+
+        return ret;
+    }
+}
