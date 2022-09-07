0481af324e8599bcf73b9a2a7a4bf67678be44dd
zhengjianfeng <zhengjianfeng@github.com>
2022-09-07 10:00:02 +0800

    1592. 重新排列单词间的空格

---
+++ Arithmetic/LeetCode/src/s1592/Solution.java
@@ -0,0 +1,45 @@
+package s1592;
+
+import java.util.StringJoiner;
+
+/**
+ * 1592. 重新排列单词间的空格
+ */
+class Solution {
+    public String reorderSpaces(String text) {
+        // 单词的数目
+        String[] word = text.trim().split(" {1,}");
+        int wordNum = word.length;
+        // 空格的数量
+        int spaceNum = text.length() - text.replace(" ", "").length();
+        // 间隔空格的数量
+        int s = 0;
+        if (wordNum > 1) {
+            s = spaceNum / (wordNum - 1);
+        } else {
+            s = spaceNum;
+        }
+        String space = "";
+        for (int i = 0; i < s; i++) {
+            space += " ";
+        }
+        String ans = "";
+        for (int i = 0; i < wordNum; i++) {
+            ans += word[i];
+            if (i != wordNum - 1) {
+                ans += space;
+                spaceNum = spaceNum - s;
+            }
+        }
+        for (int i = 0; i < spaceNum; i++) {
+            ans += " ";
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String text = " a ";
+        System.out.println(new Solution().reorderSpaces(text));
+    }
+}
