c092e6cb5988bb5199714c3f68179d290ceef8a2
zhengjianfeng <zhengjianfeng@github.com>
2022-09-29 16:46:38 +0800

    628. 三个数的最大乘积

---
+++ Arithmetic/LeetCode/src/s0628/Solution.java
@@ -0,0 +1,14 @@
+package s0628;
+
+import java.util.Arrays;
+
+/**
+ *  628. 三个数的最大乘积
+ */
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 3] * nums[n - 2] * nums[n - 1]);
+    }
+}
