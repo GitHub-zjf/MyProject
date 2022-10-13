04795db90f4417302d8cde6b8f35d21acd37b255
zhengjianfeng <zhengjianfeng@github.com>
2022-10-13 15:52:18 +0800

    1103. 分糖果 II

---
+++ Arithmetic/LeetCode/src/s1103/Solution.java
@@ -0,0 +1,23 @@
+package s1103;
+
+/**
+ * 1103. ·ÖÌÇ¹û II
+ */
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] ans = new int[num_people];
+        int i = 0;
+        int candy = 1;
+        while (candies > 0) {
+            if (candies < candy) {
+                ans[i] = ans[i] + candies;
+                break;
+            }
+            ans[i] = ans[i] + candy;
+            i = (i + 1) % num_people;
+            candies = candies - candy;
+            candy++;
+        }
+        return ans;
+    }
+}
