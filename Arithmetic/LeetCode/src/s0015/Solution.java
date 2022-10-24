f7a991efbe776dbe224b68237e9a6aec76139f6c
zhengjianfeng <zhengjianfeng@github.com>
2022-10-24 10:18:34 +0800

    15. 三数之和  * 排序 + 双指针

--- Arithmetic/LeetCode/src/s0015/Solution.java
+++ Arithmetic/LeetCode/src/s0015/Solution.java
@@ -7,52 +7,46 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-// 夭折
-class Solution {
-	public List<List<Integer>> threeSum(int[] nums) {
-		int len = nums.length;
-		if (len < 3) {
-			return new ArrayList<>();
-		}
-		Set<String> set = new LinkedHashSet<>();
-		// 存储两数之和
-		Map<int[], Integer> map = new HashMap<int[], Integer>();
-		for (int i = 0; i < len; i++) {
-			for (int j = 0; j < len; j++) {
-				if (i != j) {
-					int[] ints = new int[] { i, j };
-					map.put(ints , nums[i] + nums[j] );
-				}
-			}
-		}
-		// 寻找相反数
-		for (int i = 0; i < len; i++) {
-			Set<int[]> keySet = map.keySet();
-			for (int[] key : keySet) {
-				int value = map.get(key);
-				if (nums[i] + value == 0) {
-					if (i != key[0] && i != key[1]) {
-						int[] temp = new int[] { nums[i], nums[key[0]], nums[key[1]] };
-						Arrays.sort(temp);
-						set.add(temp[0] + ":" + temp[1] + ":" + temp[2]);
-					}
-				}
-			}
-		}
-		List<List<Integer>> list = new ArrayList<>();
-		for (String s : set) {
-			String[] split = s.split(":");
-			List<Integer> list2 = new ArrayList<>();
-			list2.add(Integer.parseInt(split[0]));
-			list2.add(Integer.parseInt(split[1]));
-			list2.add(Integer.parseInt(split[2]));
-			list.add(list2);
-		}
-		return list;
-	}
 
-	public static void main(String[] args) {
-		int[] nums = new int[] { 3, 0, -2, -1, 1, 2 };
-		new Solution().threeSum(nums);
-	}
+/**
+ * 15. 三数之和
+ */
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int len = nums.length;
+        if (len < 3) {
+            return new ArrayList<>();
+        }
+        Set<List<Integer>> set = new LinkedHashSet<>();
+        // 排序 + 双指针
+        Arrays.sort(nums);
+        for (int i = 0; i < len; i++) {
+            int first = nums[i];
+            // 如果当前的第一个数大于0，由于排序，后续加起来不可能等于0
+            if (first > 0) {
+                break;
+            }
+            int left = i + 1, right = len - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right] + first;
+                if (sum == 0) {
+                    set.add(Arrays.asList(nums[left], nums[right], first));
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        List<List<Integer>> list = new ArrayList<>(set);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 0, -2, -1, 1, 2};
+        new Solution().threeSum(nums);
+    }
 }
