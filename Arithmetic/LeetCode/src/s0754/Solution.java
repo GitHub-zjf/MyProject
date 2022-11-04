d85376e98bb49b18a50a006602fd8ac3cc04d72e
zhengjianfeng <zhengjianfeng@github.com>
2022-11-04 17:29:13 +0800

    754. 到达终点数字

---
+++ Arithmetic/LeetCode/src/s0754/Solution.java
@@ -0,0 +1,16 @@
+package s0754;
+
+/**
+ * 754. 到达终点数字
+ */
+class Solution {
+    public int reachNumber(int target) {
+        target = Math.abs(target);
+        int k = 0;
+        while (target > 0) {
+            k++;
+            target -= k;
+        }
+        return target % 2 == 0 ? k : k + 1 + k % 2;
+    }
+}
