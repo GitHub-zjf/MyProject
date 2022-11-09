ece24311329dc4860602f0f96ab2b62dbfa18f19
zhengjianfeng <zhengjianfeng@github.com>
2022-11-09 16:27:12 +0800

    1684. 统计一致字符串的数目

--- Arithmetic/LeetCode/src/s1684/Solution.java
+++ Arithmetic/LeetCode/src/s1684/Solution.java
@@ -1,25 +1,56 @@
 package s1684;
 
+/**
+ *  1684. 统计一致字符串的数目
+ */
 class Solution {
-	public int countConsistentStrings(String allowed, String[] words) {
-		int count = 0;
-		for (String word : words) {
-			boolean isAdd = true;
-			String[] split = word.split("");
-			for (String s : split) {
-				if(!allowed.contains(s)){
-					isAdd = false;
-					break;
-				}
-			}
-			if(isAdd){
-				count++;
-			}
-		}
-		return count;
-	}
+    /**
+     * 2021.04.22
+     *
+     * @param allowed
+     * @param words
+     * @return
+     */
+    public int countConsistentStrings(String allowed, String[] words) {
+        int count = 0;
+        for (String word : words) {
+            boolean isAdd = true;
+            String[] split = word.split("");
+            for (String s : split) {
+                if (!allowed.contains(s)) {
+                    isAdd = false;
+                    break;
+                }
+            }
+            if (isAdd) {
+                count++;
+            }
+        }
+        return count;
+    }
 
-	public static void main(String[] args) {
+    /**
+     * 2022-11-08
+     *
+     * @param allowed
+     * @param words
+     * @return
+     */
+    public int countConsistentStrings2(String allowed, String[] words) {
+        int count = 0;
+        String[] split = allowed.split("");
+        for (String word : words) {
+            for (String s : split) {
+                word = word.replaceAll(s, "");
+            }
+            if (word.length() == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
 
-	}
+    public static void main(String[] args) {
+        new Solution().countConsistentStrings2("ab", new String[]{"aaab"});
+    }
 }
