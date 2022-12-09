097d7d4f76fce50a5d330c95ecd644c7024d74ee
zhengjianfeng <zhengjianfeng@github.com>
2022-12-09 10:11:42 +0800

    1780. 判断一个数字是否可以表示成三的幂的和

--- Arithmetic/LeetCode/src/s1780/Solution.java
+++ Arithmetic/LeetCode/src/s1780/Solution.java
@@ -1,10 +1,17 @@
 package s1780;
+
+/**
+ * 1780. 判断一个数字是否可以表示成三的幂的和
+ */
 class Solution {
     public boolean checkPowersOfThree(int n) {
-    	return false;
+        while (n != 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
     }
-    
-    public static void main(String[] args) {
-		System.out.println(Math.pow(3, 16));
-	}
+
 }
