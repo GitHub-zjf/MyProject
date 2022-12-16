6c28463c6e54599f5540553d91043adf9acc10fa
zhengjianfeng <zhengjianfeng@github.com>
2022-12-16 09:29:03 +0800

    1785. 构成特定和需要添加的最少元素

---
+++ Arithmetic/LeetCode/src/s1785/Solution.java
@@ -0,0 +1,16 @@
+package s1785;
+
+/**
+ * 1785. 构成特定和需要添加的最少元素
+ */
+class Solution {
+    public int minElements(int[] nums, int limit, int goal) {
+        long sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        long diff = Math.abs(sum - goal);
+        // 向上取整
+        return (int) Math.ceil((double) diff / limit);
+    }
+}
