93f3e74d7ec0ae5f7b2d02382037293fba511f24
zhengjianfeng <zhengjianfeng@github.com>
2022-12-23 10:44:11 +0800

    2011. 执行操作后的变量值

--- Arithmetic/LeetCode/src/s2011/Solution.java
+++ Arithmetic/LeetCode/src/s2011/Solution.java
@@ -1,9 +1,13 @@
 package s2011;
+
+/**
+ *  2011. 执行操作后的变量值
+ */
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
     	int ret = 0;
     	for(String operation:operations){
-    		if(operation.contains("++")){
+    		if(operation.contains("+")){
     			ret++;
     		}else {
 				ret--;
