e101567c2e4095ae9b1322a6d10f41178053cf60
zhengjianfeng <zhengjianfeng@github.com>
2023-01-10 15:53:18 +0800

    771. 宝石与石头

---
+++ Arithmetic/LeetCode/src/s0771/Solution.java
@@ -0,0 +1,14 @@
+package s0771;
+
+/**
+ * 771. 宝石与石头
+ */
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int len = stones.length();
+        for (int i = 0; i < jewels.length(); i++) {
+            stones = stones.replace(String.valueOf(jewels.charAt(i)), "");
+        }
+        return len - stones.length();
+    }
+}
