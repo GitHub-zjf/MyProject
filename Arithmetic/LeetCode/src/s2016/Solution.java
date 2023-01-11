87e629831b43260fee4b3d2d6d403552ff218c24
zhengjianfeng <zhengjianfeng@github.com>
2023-01-11 16:55:58 +0800

    2016. 增量元素之间的最大差值

---
+++ Arithmetic/LeetCode/src/s2016/Solution.java
@@ -0,0 +1,19 @@
+package s2016;
+
+/**
+ * 2016. 增量元素之间的最大差值
+ */
+class Solution {
+    public int maximumDifference(int[] nums) {
+        int n = nums.length;
+        int max = -1, preMin = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > preMin) {
+                max = Math.max(max, nums[i] - preMin);
+            } else {
+                preMin = nums[i];
+            }
+        }
+        return max;
+    }
+}
