1775c2a069cd7b92dc5edac8dc6c34d44b1a20eb
zhengjianfeng <zhengjianfeng@github.com>
2023-01-12 15:08:27 +0800

    2154. 将找到的值乘以 2

---
+++ Arithmetic/LeetCode/src/s2154/Solution.java
@@ -0,0 +1,21 @@
+package s2154;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  2154. 将找到的值乘以 2
+ */
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<>(len);
+        for (int num : nums) {
+            map.put(num, num);
+        }
+        while (map.get(original) != null) {
+            original *= 2;
+        }
+        return original;
+    }
+}
