f7ad086d3ba66316bcd5da55fc9d67103226436c
zhengjianfeng <zhengjianfeng@github.com>
2023-02-03 15:48:38 +0800

    2535. 数组元素和与数字和的绝对差

---
+++ Arithmetic/LeetCode/src/s2535/Solution.java
@@ -0,0 +1,18 @@
+package s2535;
+
+/**
+ * 2535. 数组元素和与数字和的绝对差
+ */
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int sum1 = 0, sum2 = 0;
+        for (int num : nums) {
+            sum1 += num;
+            while (num != 0) {
+                sum2 += num % 10;
+                num /= 10;
+            }
+        }
+        return Math.abs(sum1 - sum2);
+    }
+}
