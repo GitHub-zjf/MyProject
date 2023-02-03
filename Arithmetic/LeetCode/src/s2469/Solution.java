7bfb3882066f39d7cc7bfee68fcaa4376a431f65
zhengjianfeng <zhengjianfeng@github.com>
2023-02-03 15:39:04 +0800

    2469. 温度转换

---
+++ Arithmetic/LeetCode/src/s2469/Solution.java
@@ -0,0 +1,13 @@
+package s2469;
+
+/**
+ *  2469. ÎÂ¶È×ª»»
+ */
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double[] ans = new double[2];
+        ans[0] = celsius + 273.15;
+        ans[1] = celsius * 1.8 + 32;
+        return ans;
+    }
+}
