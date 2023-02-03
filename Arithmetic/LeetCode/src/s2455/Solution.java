aba3fc588b499b6662e7cc99da38ba39927c9500
zhengjianfeng <zhengjianfeng@github.com>
2023-02-03 16:47:02 +0800

    2455. 可被三整除的偶数的平均值

---
+++ Arithmetic/LeetCode/src/s2455/Solution.java
@@ -0,0 +1,18 @@
+package s2455;
+
+/**
+ *  2455. 可被三整除的偶数的平均值
+ */
+class Solution {
+    public int averageValue(int[] nums) {
+        int sum = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num % 6 == 0) {
+                sum += num;
+                count++;
+            }
+        }
+        return count == 0 ? 0 : sum / count;
+    }
+}
