db9cf3568ec7476393660b28237dfe8ed7810eab
zhengjianfeng <zhengjianfeng@github.com>
2023-02-06 09:31:23 +0800

    2331. 计算布尔二叉树的值

---
+++ Arithmetic/LeetCode/src/s2331/Solution.java
@@ -0,0 +1,37 @@
+package s2331;
+
+/**
+ * 2331. 计算布尔二叉树的值
+ */
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if (root.left == null) {
+            return root.val == 1;
+        }
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        } else {
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
