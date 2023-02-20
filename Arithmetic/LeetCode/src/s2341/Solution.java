38e643583e4a53508a96f690a2b98a96b44156e8
zhengjianfeng <zhengjianfeng@github.com>
2023-02-20 17:45:29 +0800

    2341. 数组能形成多少数对

---
+++ Arithmetic/LeetCode/src/s2341/Solution.java
@@ -0,0 +1,21 @@
+package s2341;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 2341. 数组能形成多少数对
+ */
+class Solution {
+    public int[] numberOfPairs(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.get(num) % 2 == 0) {
+                count++;
+            }
+        }
+        return new int[]{count, nums.length - count * 2};
+    }
+}
