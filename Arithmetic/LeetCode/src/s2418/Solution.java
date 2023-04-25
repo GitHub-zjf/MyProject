2348ca492733bad0bf91e7a1861d4d584795645f
zhengjianfeng <zhengjianfeng@github.com>
2023-04-25 16:05:22 +0800

    2418. 按身高排序

---
+++ Arithmetic/LeetCode/src/s2418/Solution.java
@@ -0,0 +1,22 @@
+package s2418;
+
+import java.util.*;
+
+/**
+ *  2418. ∞¥…Ì∏ﬂ≈≈–Ú
+ */
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = names.length;
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
+        }
+        Arrays.sort(indices, (a, b) -> heights[b] - heights[a]);
+        String[] res = new String[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = names[indices[i]];
+        }
+        return res;
+    }
+}
