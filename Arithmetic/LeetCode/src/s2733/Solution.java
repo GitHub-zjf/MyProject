04c869e672cf94a435c6ea7a291ff14cbee7e35c
zhengjianfeng <zhengjianfeng@github.com>
2023-07-05 15:16:37 +0800

    2733. 既不是最小值也不是最大值

---
+++ Arithmetic/LeetCode/src/s2733/Solution.java
@@ -0,0 +1,16 @@
+package s2733;
+
+import java.util.Arrays;
+
+/**
+ * 2733. 既不是最小值也不是最大值
+ */
+class Solution {
+    public int findNonMinOrMax(int[] nums) {
+        if (nums.length <= 2) {
+            return -1;
+        }
+        Arrays.sort(nums, 0, 3);
+        return nums[1];
+    }
+}
