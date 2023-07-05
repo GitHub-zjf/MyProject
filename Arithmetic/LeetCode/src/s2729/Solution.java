dccf4181d65e02fafc551f2a7d2ac1e259c4d47b
zhengjianfeng <zhengjianfeng@github.com>
2023-07-05 15:32:07 +0800

    2729. 判断一个数是否迷人

---
+++ Arithmetic/LeetCode/src/s2729/Solution.java
@@ -0,0 +1,32 @@
+package s2729;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * 2729. 判断一个数是否迷人
+ */
+class Solution {
+    public boolean isFascinating(int n) {
+        if (n < 123 || n > 329) {
+            return false;
+        }
+        int mask = 0;
+        for (char c : ("" + n + (n * 2) + (n * 3)).toCharArray()) {
+            mask |= 1 << (c - '0');
+        }
+        return mask == (1 << 10) - 2;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        for (int i = 100; i < 999; i++) {
+            if (solution.isFascinating(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+}
