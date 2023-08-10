8b68e4c034404a02e9deeb1297cabf9f7f149e60
zhengjianfeng <zhengjianfeng@github.com>
2023-08-10 16:51:22 +0800

    剑指 Offer 05. 替换空格

---
+++ Arithmetic/LeetCode/src/jian_zhi_offer/I/s0005/Solution.java
@@ -0,0 +1,19 @@
+package jian_zhi_offer.I.s0005;
+
+/**
+ * ½£Ö¸ Offer 05. Ìæ»»¿Õ¸ñ
+ */
+class Solution {
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) == ' ') {
+                sb.append("%20");
+            }else{
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
