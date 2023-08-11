e02e18dbf98beadc3089fe2bb265a847756f351a
zhengjianfeng <zhengjianfeng@github.com>
2023-08-11 16:46:20 +0800

    1572. 矩阵对角线元素的和

---
+++ Arithmetic/LeetCode/src/daily/s1572/Solution.java
@@ -0,0 +1,19 @@
+package daily.s1572;
+
+/**
+ * 1572. 矩阵对角线元素的和
+ */
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    sum += mat[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+}
